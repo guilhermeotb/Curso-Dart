62c1c27a5631c4d2b5a88b9968ec3446aa47087b
Pereira <pereira@eb.mil.br>
2026-02-12 14:27:08 -0300

    #5 Criação do projeto de notas e  esqueleto da função getComando

---
+++ notas/bin/notas.dart
@@ -0,0 +1,11 @@
+
+void main() {
+
+}
+
+String getComando()  {
+  print("Digite um Comando: 1- Adicionar nota, 2 - Listar Notas, 3 - Sair");
+  List<String> comandos = <Strings>["1","2","3"];
+  String? entrada = "";
+
+}
