996e9e6568b6e6a85de934d7e3c27fc537ae3dfc
Pereira <pereira@eb.mil.br>
2026-02-12 14:35:51 -0300

    #5 Implementação da função getComando com tratamento de erros

--- notas/bin/notas.dart
+++ notas/bin/notas.dart
@@ -1,11 +1,19 @@
-
+import 'dart:io';
 void main() {
-
+getComando();
 }
 
 String getComando()  {
   print("Digite um Comando: 1- Adicionar nota, 2 - Listar Notas, 3 - Sair");
-  List<String> comandos = <Strings>["1","2","3"];
+  List<String> comandos = <String>["1","2","3"];
   String? entrada = "";
 
+  entrada = stdin.readLineSync();
+
+  if(entrada == null || !comandos.contains(entrada) ) {
+    print("Comando inválido");
+    getComando();
+  }else { print("Passou no teste de erro");}
+
+  return entrada!;
 }
